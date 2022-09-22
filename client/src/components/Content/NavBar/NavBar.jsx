c0475238aea708b0c1ff0c2a53413872787c8056
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2022-09-22 21:08:10 +0530

    Mobile-Version 1.0

--- client/src/components/Content/NavBar/NavBar.jsx
+++ client/src/components/Content/NavBar/NavBar.jsx
@@ -6,10 +6,12 @@ const NavBar = ()=>{
         <div className="container">
             <div className = "content">
                 <div className="left-nav"><h1>theCoderNoob</h1></div>
-                <div className="right-nav">                
-                <h2>Projects</h2>
-                <h2>Posts</h2>
-                 <h2>About</h2>
+                <div className="right-nav">       
+                <div className="right-nav-responsive">
+                    <h2>Projects</h2>
+                    <h2>Posts</h2>
+                     <h2>About</h2>
+                </div>         
              </div>
             </div>
         </div>
