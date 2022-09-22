fcef49ced790aff24087303375e51e50533509a3
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2022-09-22 14:43:16 +0530

    HomePage version 1.0

--- client/src/components/FooterContainer/Footer.jsx
+++ client/src/components/FooterContainer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import '../FooterContainer/FooterStyle.scss';
+import githubimage from "../../image/GitHubImage.png";
+import linkedIn from "../../image/linkedIn.png";
+import twitter from "../../image/twitter.png";
+import funny from "../../image/funny.png";
+import funny1 from "../../image/funny1.png";
 
 const Footer = ()=>{
     return(
@@ -7,16 +12,16 @@ const Footer = ()=>{
             <div className="Footer-Content">
                 <div className="Link-Container">
                     <ul>
-                        <li><a href="#">GitHub</a></li>
-                        <li><a href="#">LinkedIn</a></li>
-                        <li><a href="#">Twitter</a></li>
+                        <li><img id="github-image-footer" src={githubimage}/><a href="#">GitHub</a></li>
+                        <li><img id="linkedIn-image-footer" src={linkedIn}/><a href="#">LinkedIn</a></li>
+                        <li><img id="twitter-image-footer" src={twitter}/><a href="#">Twitter</a></li>
                     </ul>
                 </div>
                 <div className="Ending-Section-Container">
                     <div className="Ending-Section-Content">
                         <p><span id="copyright">© Tejas Kulkarni 2022</span></p>
-                        <p>I don't track you</p>
-                        <p>Last Build</p>
+                        <p><img id="funny-image-footer" src={funny} />I don't track you</p>
+                        <p><img id="funny-image-footer" src={funny1} />It’s not a bug. It’s an undocumented feature!</p>
                     </div>
                 </div>
             </div>
