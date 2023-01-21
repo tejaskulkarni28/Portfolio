1df190fe0968bc24d06d6836858c2f5dcd4a675d
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-01-21 12:11:35 +0530

    added my photo

--- client/src/components/Content/LeftSideContent/LeftSideContent.jsx
+++ client/src/components/Content/LeftSideContent/LeftSideContent.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './LeftSideStyle.scss';
+import myPhoto from "../../../image/IMG_1883.png";
 
 const LeftSideContent = ()=>{
     return(
         <div className="LeftContainer">
             <div className="LeftContent">
+            <img id="myPhoto" src={myPhoto}/>
                 <h3><span id="hii-there">Hii There! 👋</span><br/>I'M <span id="typingName">Tejas Kulkarni.</span></h3>
                 <p>Post-Grad Student in Master of Computer Applications <br/>at MIT-WPU.</p>
-                <p className="p-child">I'm a <span id="span">software developer.</span> I help developers build stuff,<br/> learn things, and love what they do through blog posts, tutorial videos, live streaming about tech and building open source projects.</p>
+                <p className="p-child">I'm a <span id="span">software developer.</span><br/> I help developers build 🔥cool stuff🔥,<br/> learn things, and love what they do through blog posts, tutorial videos, live streaming about tech and building open source projects.</p>
             </div>
         </div>
     )
