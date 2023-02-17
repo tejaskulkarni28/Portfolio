7f75190fdfd1c5354fe21daaabb4b35c517b184c
theCoderNoob <81331536+tejaskulkarni28@users.noreply.github.com>
2023-02-17 12:40:14 +0530

    token updated

--- client/src/security/token.js
+++ client/src/security/token.js
@@ -1,7 +1,7 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-    auth: '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'
+    auth: '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'
   });
 
   // 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
