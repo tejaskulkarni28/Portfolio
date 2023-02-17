1e7eb80f61a32a38de9e052b2dee8b12866b6af4
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-02-17 11:34:17 +0530

    token updated

--- client/src/security/token.js
+++ client/src/security/token.js
@@ -1,5 +1,7 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-    auth: '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'
+    auth: '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'
   });
+
+  // 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
